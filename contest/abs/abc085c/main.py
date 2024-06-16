04b285946bcad5fb1b6c528dc509bfeb556f284a
tomoya-n <s266227@wakayama-u.ac.jp>
2024-06-17 01:21:10 +0900

    solve abs085c

--- contest/abs/abc085c/main.py
+++ contest/abs/abc085c/main.py
@@ -1,6 +1,24 @@
 def main():
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    X = [list(map(int, input().split())) for _ in range(N)]
+    N, Y = map(int, input().split())
 
+    T_TH = 10000
+    F_TH = 5000
+    O_TH = 1000
+
+    x = y = z = 0
+
+    for t in range(N + 1):
+        x = t
+        for f in range(N + 1):
+            y = f
+            z = N - x - y
+            if z < 0:
+                continue
+            ans = x * T_TH + y * F_TH + z * O_TH
+            if ans == Y:
+                print(f"{x} {y} {z}")
+                exit()
+
+                            
+    print(f"{-1} {-1} {-1}")
 main()
