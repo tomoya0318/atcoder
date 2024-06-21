5cbf9610574c3dfe66c715ffa7684882a707bb5f
tomoya-n <s266227@wakayama-u.ac.jp>
2024-06-22 00:43:29 +0900

    solve c

--- contest/abs/abc086c/main.py
+++ contest/abs/abc086c/main.py
@@ -1,6 +1,25 @@
 def main():
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    X = [list(map(int, input().split())) for _ in range(N)]
+    N = int(input())
+    T, X, Y = [], [], []
+    t_old = x_old = y_old = 0
+
+    for _ in range(N):
+        ti, xi, yi = map(int, input().split())
+        T.append(ti)
+        X.append(xi)
+        Y.append(yi)
+
+    for t, x, y in zip(T, X, Y):
+        abel = abs(t_old - t)
+        dist = abs(x_old - x) + abs(y_old - y)
+        if (t + x + y) % 2 != 0 or abel < dist:
+            print("No")
+            exit()
+            
+        t_old = t
+        x_old = x
+        y_old = y
+    
+    print("Yes")
 
 main()
